2471936decc8d91b52f6d906eac94d0a44d83c87
Shivam Shukla <shivam.shukla@lsinextgen.com>
2024-06-17 14:18:00 +0530

    first commit

---
+++ 1-intro.js
@@ -0,0 +1,11 @@
+const amount = 9;
+
+if (amount < 10) {
+    console.log('small number');
+} else {
+    console.log('large number');
+}
+
+console.log('hey its my first node app !!!')
+
+// in termil run node 1-intro.js to run this file
---
+++ 4-name.js
@@ -0,0 +1,11 @@
+// loca 
+const secret = 'SUPER SECRET'
+
+// share
+
+const john = 'john'
+const peter = 'peter'
+
+console.log(module);
+
+module.exports = { john, peter }
